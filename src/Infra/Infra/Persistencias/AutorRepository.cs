9da25c7caca8fa0ccdc6fc57c4b9ffdfd9788b3f
Ancelmo Luiz <ancelmosantos@setic.ro.gov.br>
2021-04-29 22:09:58 -0400

    início interfaces repository

---
+++ src/Infra/Infra/Persistencias/AutorRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dominio.Entidades;
+using Infra.Contexto;
+using Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Persistencias
+{
+    public class AutorRepository : IAutorRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public AutorRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task Criar(Autor autor)
+        {
+            _dataContext.Autores.Add(autor);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task Alterar(Autor autor)
+        {
+            _dataContext.Update(autor);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task Excluir(Autor autor)
+        {
+            _dataContext.Remove(autor);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task<Autor> BuscarPorId(int id)
+        {
+            var autor = await _dataContext.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            return autor;
+        }
+
+        public async Task<IEnumerable<Autor>> ListarTodosAutores()
+        {
+            return await _dataContext.Autores.AsNoTracking().ToListAsync();
+        }
+    }
+}
---
+++ src/Infra/Infra/Repositories/IAutorRepository.cs
@@ -0,0 +1,15 @@
+﻿using Dominio.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infra.Repositories
+{
+    public interface IAutorRepository
+    {
+        Task Criar(Autor autor);
+        Task Alterar(Autor autor);
+        Task Excluir(Autor autor);
+        Task<Autor> BuscarPorId(int id);
+        Task<IEnumerable<Autor>> ListarTodosAutores();
+    }
+}
